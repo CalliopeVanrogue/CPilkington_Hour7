81dc9abafce3861b3954844c234d51df1c9234b2
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-10-06 17:33:39 -0700

    Birthday script complete

--- Assets/Scripts/MyBirthday.cs
+++ Assets/Scripts/MyBirthday.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class MyBirthday : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     for (int julyDays = 1; julyDays <= 31; julyDays++)
+	{
+	if (julyDays == 24)
+	{
+	Debug.Log("Its my Birthday");
+	}
+	else
+	{
+	Debug.Log(julyDays);
+	}
+	}   
     }
 
     // Update is called once per frame
